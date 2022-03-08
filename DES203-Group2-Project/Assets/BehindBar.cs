0b2218ea4fbb53f8cb372a6c50fca81b289f0a8a
Lewis Trainer <lewis.trainer@hotmail.co.uk>
2022-03-08 14:34:52 +0000

    Improved 2D colliders

--- DES203-Group2-Project/Assets/BehindBar.cs
+++ DES203-Group2-Project/Assets/BehindBar.cs
@@ -7,7 +7,7 @@ public class BehindBar : MonoBehaviour
 {
 
     // Update is called once per frame
-    private void OnTriggerEnter2D(Collider2D other)
+    public void MoveToBar()
     {
         SceneManager.LoadScene(2);
     }
